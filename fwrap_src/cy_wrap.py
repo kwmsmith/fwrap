ae6a5c64bc315d82e4e5a4dadb0c76779b825ea5
Kurt Smith <kwsmith1@wisc.edu>
2010-01-27 23:05:46 -0600

    added stubs in cy_wrap.py and tests

--- fwrap_src/cy_wrap.py
+++ fwrap_src/cy_wrap.py
@@ -1,3 +1,18 @@
 
 
+def generate_pyx(program_unit_list, buf):
+    buf.write('''
+cimport DP_c
 
+cpdef api DP_c.fwrap_default_int empty_func():
+    return DP_c.empty_func_c()
+'''
+)
+
+def generate_pxd(program_unit_list, buf):
+    buf.write('''
+cimport DP_c
+
+cpdef api DP_c.fwrap_default_int empty_func()
+'''
+)
--- fwrap_src/tests/test_cy_wrap.py
+++ fwrap_src/tests/test_cy_wrap.py
@@ -2,13 +2,32 @@ from fwrap_src import cy_wrap
 from fwrap_src import pyf_iface as pyf
 from cStringIO import StringIO
 
-def test_empty_func():
-    empty_func = pyf.function(name='empty_func',
-                        args=(),
-                        return_type=pyf.default_integer)
-    buf = StringIO()
-    cy_wrap.generate([empty_func], buf)
-    cy_wrapper = '''
-#XXX cpdef int empty_func():
-'''
+from nose.tools import ok_, eq_, set_trace
 
+class test_empty_func(object):
+
+    def setup(self):
+        self.empty_func = pyf.function(name='empty_func',
+                            args=(),
+                            return_type=pyf.default_integer)
+        self.buf = StringIO()
+
+    def test_empty_func_pyx_wrapper(self):
+        cy_wrap.generate_pyx([self.empty_func], self.buf)
+        pyx_wrapper = '''
+cimport DP_c
+
+cpdef api DP_c.fwrap_default_int empty_func():
+    return DP_c.empty_func_c()
+'''.splitlines()
+        eq_(pyx_wrapper, self.buf.getvalue().splitlines())
+    
+
+    def test_empty_func_pxd_wrapper(self):
+        cy_wrap.generate_pxd([self.empty_func], self.buf)
+        pxd_wrapper = '''
+cimport DP_c
+
+cpdef api DP_c.fwrap_default_int empty_func()
+'''.splitlines()
+        eq_(pxd_wrapper, self.buf.getvalue().splitlines())
