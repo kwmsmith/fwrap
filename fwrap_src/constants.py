4a4690de4125797c468d13fb9d574841c8d4f074
kwmsmith <kwmsmith@gmail.com>
2010-05-13 21:43:36 -0500

    added & changed some filenames in constants.py

--- fwrap_src/constants.py
+++ fwrap_src/constants.py
@@ -2,9 +2,14 @@ PROC_SUFFIX_TMPL = "%s_c"
 
 KTP_MOD_NAME = "fwrap_ktp_mod"
 KTP_HEADER_NAME = "fwrap_ktp_header.h"
-KTP_PXD_HEADER_NAME = "fwrap_ktp"
+KTP_PXD_HEADER_NAME = "fwrap_ktp.pxd"
 
 FC_HDR_TMPL = "%s_fc.h"
-FC_PXD_TMPL = "%s_fc"
+FC_PXD_TMPL = "%s_fc.pxd"
+FC_F_TMPL = "%s_fc.f90"
 
+CY_PXD_TMPL = "%s.pxd"
+CY_PYX_TMPL = "%s.pyx"
+
+GENCONFIG_NAME = "genconfig.f90"
 MAP_FILE_NAME = "fwrap_type_map.out"
