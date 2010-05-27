e6895b232e83594deb62fba704582105f8dc437c
kwmsmith <kwmsmith@gmail.com>
2010-05-27 16:58:10 -0500

    added all_ints test
    --HG--
    rename : tests/bugs/all_ints.f95 => tests/run/all_ints.f95

--- tests/run/all_ints.f95
+++ tests/run/all_ints.f95
@@ -4,8 +4,8 @@
       integer, intent(in) :: i1
       integer, intent(inout) :: i2
       integer, intent(out) :: i3
-      i3 = i3 + i1 - i1
-      i2 = i2 + i3 - i3
+      i3 = i1 + i1
+      i2 = i2 + 2*i3
       end subroutine
       subroutine int_x_len(i4,i5,i6,i7,i8,i9,i10,i11,i12,i13,i14,i15)
       implicit none
@@ -45,25 +45,25 @@
       i9 = i7 + i8
       i12 = i10 + i11
       end subroutine
-      subroutine int_kind_call(i1,i2,i3)
-      implicit none
-      integer(kind=kind(0)), intent(in) :: i1
-      integer(kind=kind(0)), intent(inout) :: i2
-      integer(kind=kind(0)), intent(out) :: i3
-      i3 = i1 + i2
-      end subroutine
-      subroutine int_sik_call(i1,i2,i3,i4,i5,i6,i7,i8,i9)
-      implicit none
-      integer(kind=selected_int_kind(1)), intent(in) :: i1
-      integer(kind=selected_int_kind(1)), intent(inout) :: i2
-      integer(kind=selected_int_kind(1)), intent(out) :: i3
-      integer(kind=selected_int_kind(5)), intent(in) :: i4
-      integer(kind=selected_int_kind(5)), intent(inout) :: i5
-      integer(kind=selected_int_kind(5)), intent(out) :: i6
-      integer(kind=selected_int_kind(10)), intent(in) :: i7
-      integer(kind=selected_int_kind(10)), intent(inout) :: i8
-      integer(kind=selected_int_kind(10)), intent(out) :: i9
-      i3 = i1 + i2
-      i6 = i4 + i5
-      i9 = i7 + i8
-      end subroutine
+      ! subroutine int_kind_call(i1,i2,i3)
+      ! implicit none
+      ! integer(kind=kind(0)), intent(in) :: i1
+      ! integer(kind=kind(0)), intent(inout) :: i2
+      ! integer(kind=kind(0)), intent(out) :: i3
+      ! i3 = i1 + i2
+      ! end subroutine
+      ! subroutine int_sik_call(i1,i2,i3,i4,i5,i6,i7,i8,i9)
+      ! implicit none
+      ! integer(kind=selected_int_kind(1)), intent(in) :: i1
+      ! integer(kind=selected_int_kind(1)), intent(inout) :: i2
+      ! integer(kind=selected_int_kind(1)), intent(out) :: i3
+      ! integer(kind=selected_int_kind(5)), intent(in) :: i4
+      ! integer(kind=selected_int_kind(5)), intent(inout) :: i5
+      ! integer(kind=selected_int_kind(5)), intent(out) :: i6
+      ! integer(kind=selected_int_kind(10)), intent(in) :: i7
+      ! integer(kind=selected_int_kind(10)), intent(inout) :: i8
+      ! integer(kind=selected_int_kind(10)), intent(out) :: i9
+      ! i3 = i1 + i2
+      ! i6 = i4 + i5
+      ! i9 = i7 + i8
+      ! end subroutine
