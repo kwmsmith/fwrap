2bc87b1d81df3bff7e91feffb21c4007b89b6110
kwmsmith <kwmsmith@gmail.com>
2010-05-29 10:25:20 -0500

    moved all_complex test to run folder
    --HG--
    rename : tests/bugs/all_complex.f95 => tests/run/all_complex.f95
    rename : tests/bugs/all_complex_fwrap_doctest.py => tests/run/all_complex_fwrap_doctest.py

---
+++ tests/run/all_complex_fwrap_doctest.py
@@ -0,0 +1,10 @@
+from all_complex_fwrap import *
+
+__doc__ = u'''
+>>> complex_default(1+1j,2+2j,3+3j)
+(2.0, 3.0)
+>>> complex_x_len(10,11,12,13,14,15)
+(11.0, 12.0, 14.0, 15.0)
+>>> complex_kind_x(7,8,9,10,11,12)
+(8.0, 9.0, 11.0, 12.0)
+'''
