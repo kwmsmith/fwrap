27555a758099d88823d37fbc678d7440bce4871f
kwmsmith <kwmsmith@gmail.com>
2010-06-20 14:34:48 -0500

    added all_logicals.f95 tests; all pass
    --HG--
    rename : tests/bugs/all_logicals.f95 => tests/run/all_logicals.f95

--- tests/run/all_logicals.f95
+++ tests/run/all_logicals.f95
@@ -56,38 +56,38 @@
       i12= .false.
 
       end subroutine
-      subroutine log_kind_call(i1,i2,i3,i4,i5,i6)
-      implicit none
-      logical(kind=kind(.true.)), intent(in) :: i1
-      logical(kind=kind(.true.)), intent(inout) :: i2
-      logical(kind=kind(.true.)), intent(out) :: i3
-      logical(kind=kind(0)), intent(in) :: i4
-      logical(kind=kind(0)), intent(inout) :: i5
-      logical(kind=kind(0)), intent(out) :: i6
+      ! subroutine log_kind_call(i1,i2,i3,i4,i5,i6)
+      ! implicit none
+      ! logical(kind=kind(.true.)), intent(in) :: i1
+      ! logical(kind=kind(.true.)), intent(inout) :: i2
+      ! logical(kind=kind(.true.)), intent(out) :: i3
+      ! logical(kind=kind(0)), intent(in) :: i4
+      ! logical(kind=kind(0)), intent(inout) :: i5
+      ! logical(kind=kind(0)), intent(out) :: i6
 
-      i2= .false. .and. i1
-      i3= .false.
-      i5= .false. .and. i4
-      i6= .false.
+      ! i2= .false. .and. i1
+      ! i3= .false.
+      ! i5= .false. .and. i4
+      ! i6= .false.
 
-      end subroutine
-      subroutine log_sik_call(i1,i2,i3,i4,i5,i6,i7,i8,i9)
-      implicit none
-      logical(kind=selected_int_kind(1)), intent(in) :: i1
-      logical(kind=selected_int_kind(1)), intent(inout) :: i2
-      logical(kind=selected_int_kind(1)), intent(out) :: i3
-      logical(kind=selected_int_kind(5)), intent(in) :: i4
-      logical(kind=selected_int_kind(5)), intent(inout) :: i5
-      logical(kind=selected_int_kind(5)), intent(out) :: i6
-      logical(kind=selected_int_kind(10)), intent(in) :: i7
-      logical(kind=selected_int_kind(10)), intent(inout) :: i8
-      logical(kind=selected_int_kind(10)), intent(out) :: i9
+      ! end subroutine
+      ! subroutine log_sik_call(i1,i2,i3,i4,i5,i6,i7,i8,i9)
+      ! implicit none
+      ! logical(kind=selected_int_kind(1)), intent(in) :: i1
+      ! logical(kind=selected_int_kind(1)), intent(inout) :: i2
+      ! logical(kind=selected_int_kind(1)), intent(out) :: i3
+      ! logical(kind=selected_int_kind(5)), intent(in) :: i4
+      ! logical(kind=selected_int_kind(5)), intent(inout) :: i5
+      ! logical(kind=selected_int_kind(5)), intent(out) :: i6
+      ! logical(kind=selected_int_kind(10)), intent(in) :: i7
+      ! logical(kind=selected_int_kind(10)), intent(inout) :: i8
+      ! logical(kind=selected_int_kind(10)), intent(out) :: i9
 
-      i2= .false. .and. i1
-      i3= .false.
-      i5= .false. .and. i4
-      i6= .false.
-      i8= .false. .and. i7
-      i9= .false.
+      ! i2= .false. .and. i1
+      ! i3= .false.
+      ! i5= .false. .and. i4
+      ! i6= .false.
+      ! i8= .false. .and. i7
+      ! i9= .false.
 
-      end subroutine
+      ! end subroutine
