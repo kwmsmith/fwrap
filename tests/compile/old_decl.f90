82d29652bbd968283d111328b71c3d263610a4f5
kwmsmith <kwmsmith@gmail.com>
2010-06-21 23:08:50 -0500

    removed character argument for ifort to work

--- tests/compile/old_decl.f90
+++ tests/compile/old_decl.f90
@@ -1,11 +1,10 @@
-subroutine bar(a,b,c,d,e,f,g)
+subroutine bar(a,b,c,d,e,f)
     implicit none
     integer*4, intent(inout) :: a
     real*4, intent(inout) :: b,c
     real*8, intent(inout) :: d
     integer*8, intent(inout) :: e
     double precision, intent(inout) :: f
-    character(kind=1,len=8), intent(inout) :: g
 
     a = 1
     b = 2
@@ -13,6 +12,5 @@ subroutine bar(a,b,c,d,e,f,g)
     d = 3
     e = 4
     f = 5
-    g = 'a'
 
 end subroutine bar
