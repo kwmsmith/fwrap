5673626cc93b28a3d7ac9b301b602cf4efe21beb
kwmsmith <kwmsmith@gmail.com>
2010-07-02 12:24:12 -0500

    more character tests

--- tests/run/all_char.f90
+++ tests/run/all_char.f90
@@ -8,3 +8,16 @@
         ch = ch_in
 
       end subroutine char_arg
+
+      subroutine char_star(ch, ch_in, ch_out, ch_inout)
+        implicit none
+        character(*) ch
+        character(len=*), intent(in) :: ch_in
+        character(*), intent(out) :: ch_out
+        character(*), intent(inout) :: ch_inout
+
+        ch = ch_in
+
+        ch_out = ch_inout
+
+      end subroutine char_star
--- tests/run/all_char_fwrap_doctest.py
+++ tests/run/all_char_fwrap_doctest.py
@@ -3,4 +3,6 @@ from all_char_fwrap import *
 __doc__ = u'''
 >>> char_arg('foobar', 'ch_in')
 ('ch_in               ', 'ch_in')
+>>> char_star('aaaaa', 'bb', 'cccccccccc', 'dddddddd')
+('bb   ', 'bb', 'dddddddd  ', 'dddddddd')
 '''
