92c294aca6a5f675786e79c270ce446eb9a27a17
kwmsmith <kwmsmith@gmail.com>
2010-07-02 00:27:44 -0500

    added all_char.f90 & doctest

---
+++ tests/run/all_char.f90
@@ -0,0 +1,10 @@
+
+
+      subroutine char_arg(ch, ch_in)
+        implicit none
+        character(len=20) :: ch
+        character(*) :: ch_in
+
+        ch = ch_in
+
+      end subroutine char_arg
---
+++ tests/run/all_char_fwrap_doctest.py
@@ -0,0 +1,6 @@
+from all_char_fwrap import *
+
+__doc__ = u'''
+>>> char_arg('foobar', 'ch_in')
+('ch_in               ', 'ch_in')
+'''
