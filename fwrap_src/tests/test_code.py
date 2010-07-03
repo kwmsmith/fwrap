1ae379958e55fa6c0854dff599ff4bf0fe78b8c5
Kyle Mandli <mandli@amath.washington.edu>
2010-07-02 19:04:18 -0500

    Moved reflow test to code test module and rewrote

--- fwrap_src/tests/test_code.py
+++ fwrap_src/tests/test_code.py
@@ -56,3 +56,23 @@ def test_indent():
     ret = code.reflow_line(line, 1, 10)
     eq_(ret, [code.INDENT+line[:8]+'&', code.INDENT+'&'+line[8:16]+'&', code.INDENT+'&'+line[16:]])
     # set_trace()
+
+def test_reflow():
+    reflow_src = ("subroutine many_args(a0, a1, a2, a3, a4, a5, a6, a7, a8, ",
+                  "a9, a20, a21, a22, a23, a24, a25, a26, a27, a28, a29, a30",
+                  ", a31, a32, a33, a34, a35, a36, a37, a38, a39, a40, a41, ",
+                  "a42, a43, a44, a45, a46, a47, a48, a49)\n",
+                  "    implicit none\n",
+                  "    integer, intent(in) :: a0, a1, a2, a3, a4, a5, a6, ",
+                  "a7, a8, a9, a20, a21, a22, a23, a24, a25, a26, a27, a28, ",
+                  "a29, a30, a31, a32, a33, a34, a35, a36, a37, a38, a39, ",
+                  "a40, a41, a42, a43, a44, a45, a46, a47, a48, a49\n",
+                  "end subroutine many_args")
+    
+    buf = CodeBuffer()
+    buf.putline(reflow_fort(reflow_src))
+    for line in buf.getvalue().splitlines():
+        ok_(len(line) <= 79, "len('%s') > 79" % line)
+
+            
+    
