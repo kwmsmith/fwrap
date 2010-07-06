8e76ebddfe4e7d1404911f72a2294ed33e4a88df
kwmsmith <kwmsmith@gmail.com>
2010-07-05 19:02:11 -0500

    removed redundant complex array tests

--- tests/run/cmplx_array_fwrap_doctest.py
+++
@@ -1,9 +0,0 @@
-from cmplx_array_fwrap import *
-import numpy as np
-
-carr = np.empty((2,2), dtype=np.complex128, order='F')
-
-__doc__ = u'''\
->>> np.all(complex_array(carr) == np.array([[ 1.+2.j,  1.+2.j], [ 1.+2.j,  1.+2.j]]))
-True
-'''
