5398af716d25bd42a75ecca0d37e78bbe1f57c3c
kwmsmith <kwmsmith@gmail.com>
2010-07-09 14:33:47 -0500

    refactoring & cleanup in gen_config.py

--- fwrap_src/gen_config.py
+++ fwrap_src/gen_config.py
@@ -1,99 +1,10 @@
 from cPickle import dumps
-from fwrap_src import pyf_iface
 import constants
 
-def read_type_spec(fname):
-    from cPickle import loads
-    fh = open(fname, 'rb')
-    ds = loads(fh.read())
-    fh.close()
-    return [ConfigTypeParam(**d) for d in ds]
+INDENT = "    "
 
-def write_f_mod(fname, ctps):
-    f_out = open(fname, 'w')
-    try:
-        f_out.write('''
-module fwrap_ktp_mod
-    use iso_c_binding
-    implicit none
-''')
-        for err_name in sorted(constants.ERR_CODES):
-            f_out.write("    integer, parameter :: %s = %d\n" % (err_name, constants.ERR_CODES[err_name]))
-        for ctp in ctps:
-            for line in ctp.gen_f_mod():
-                f_out.write('    %s\n' % line)
-        f_out.write('end module fwrap_ktp_mod\n')
-    finally:
-        f_out.close()
-
-def get_c_includes(ctps):
-    used_ctp_types = set()
-    for ctp in ctps:
-        used_ctp_types.add(type(ctp))
-    includes = []
-    for tp in used_ctp_types:
-        includes.append(tp.c_includes)
-    return includes
-
-def write_header(fname, ctps):
-    h_out = open(fname, 'w')
-    try:
-        h_out.write("#ifndef %s\n" % fname.upper().replace('.','_'))
-        h_out.write("#define %s\n" % fname.upper().replace('.', '_'))
-        for err_name in sorted(constants.ERR_CODES):
-            h_out.write("#define %s %d\n" % (err_name, constants.ERR_CODES[err_name]))
-        for incl in get_c_includes(ctps):
-            if incl: h_out.write(incl+'\n')
-        for ctp in ctps:
-            for line in ctp.gen_c_typedef():
-                h_out.write(line+'\n')
-        for ctp in ctps:
-            for line in ctp.gen_c_extra():
-                h_out.write(line+'\n')
-
-        h_out.write("#endif")
-    finally:
-        h_out.close()
-
-def write_pxd(fname, h_name, ctps):
-    from cStringIO import StringIO
-    pxd_out = open(fname, 'w')
-    extern_block = StringIO()
-    try:
-        for ctp in ctps:
-            for line in ctp.gen_pxd_cimports():
-                pxd_out.write(line+'\n')
-            for line in ctp.gen_pxd_intern_typedef():
-                pxd_out.write(line+'\n')
-        for ctp in ctps:
-            for line in ctp.gen_pxd_extern_typedef():
-                extern_block.write('    '+line+'\n')
-        for ctp in ctps:
-            for line in ctp.gen_pxd_extern_extra():
-                extern_block.write('    '+line+'\n')
-        extern_block = extern_block.getvalue()
-        if extern_block.rstrip():
-            pxd_out.write('cdef extern from "%s":\n' % h_name)
-            pxd_out.write("    enum:\n")
-            for err_name in sorted(constants.ERR_CODES):
-                pxd_out.write("        %s = %d\n" %\
-                        (err_name, constants.ERR_CODES[err_name]))
-            pxd_out.write(extern_block)
-    finally:
-        pxd_out.close()
-
-def generate_type_specs(ast, buf):
-    ctps = extract_ctps(ast)
-    _generate_type_specs(ctps, buf)
-
-def _generate_type_specs(ctps, buf):
-    out_lst = []
-    for ctp in ctps:
-        out_lst.append(dict(basetype=ctp.basetype,
-                            odecl=ctp.odecl,
-                            fwrap_name=ctp.fwrap_name,
-                            lang=ctp.lang))
-    buf.write(dumps(out_lst))
+#------------------------------------------------------------------------------
+# -- Collect, store and load type specifications to / from a file ---
 
 def extract_ctps(ast):
     dtypes = set()
@@ -112,6 +23,120 @@ def ctps_from_dtypes(dtypes):
                        odecl=dtype.odecl,
                        lang=dtype.lang))
     return ret
+
+def generate_type_specs(ast, buf):
+    ctps = extract_ctps(ast)
+    _generate_type_specs(ctps, buf)
+
+def _generate_type_specs(ctps, buf):
+    out_lst = []
+    for ctp in ctps:
+        out_lst.append(dict(basetype=ctp.basetype,
+                            odecl=ctp.odecl,
+                            fwrap_name=ctp.fwrap_name,
+                            lang=ctp.lang))
+    buf.write(dumps(out_lst))
+
+def read_type_spec(fname):
+    from cPickle import loads
+    fh = open(fname, 'rb')
+    ds = loads(fh.read())
+    fh.close()
+    return [ConfigTypeParam(**d) for d in ds]
+
+#------------------------------------------------------------------------------
+# -- Write out the type information to a Fortran module, a C header and a
+#    cython .pxd
+
+def write_f_mod(fname, ctps):
+
+    def write_err_codes(f_out):
+        for err_name in sorted(constants.ERR_CODES):
+            f_out.write(INDENT+"integer, parameter :: %s = %d\n" % \
+                    (err_name, constants.ERR_CODES[err_name]))
+
+    f_out = open(fname, 'w')
+    try:
+        f_out.write('''
+module fwrap_ktp_mod
+    use iso_c_binding
+    implicit none
+''')
+        write_err_codes(f_out)
+        for ctp in ctps:
+            for line in ctp.gen_f_mod():
+                f_out.write(INDENT+'%s\n' % line)
+        f_out.write('end module fwrap_ktp_mod\n')
+    finally:
+        f_out.close()
+
+def get_c_includes(ctps):
+    used_ctp_types = set()
+    for ctp in ctps:
+        used_ctp_types.add(type(ctp))
+    includes = []
+    for tp in used_ctp_types:
+        includes.append(tp.c_includes)
+    return includes
+
+def write_header(fname, ctps):
+
+    def write_err_codes(h_out):
+        for err_name in sorted(constants.ERR_CODES):
+            h_out.write("#define %s %d\n" % \
+                    (err_name, constants.ERR_CODES[err_name]))
+
+    h_out = open(fname, 'w')
+    try:
+        h_out.write("#ifndef %s\n" % fname.upper().replace('.','_'))
+        h_out.write("#define %s\n" % fname.upper().replace('.', '_'))
+        write_err_codes(h_out)
+        for incl in get_c_includes(ctps):
+            if incl: h_out.write(incl+'\n')
+        for ctp in ctps:
+            for line in ctp.gen_c_typedef():
+                h_out.write(line+'\n')
+        for ctp in ctps:
+            for line in ctp.gen_c_extra():
+                h_out.write(line+'\n')
+
+        h_out.write("#endif")
+    finally:
+        h_out.close()
+
+def write_pxd(fname, h_name, ctps):
+
+    def write_err_codes(pxd_out):
+        pxd_out.write(INDENT+"enum:\n")
+        for err_name in sorted(constants.ERR_CODES):
+            pxd_out.write((INDENT*2)+"%s = %d\n" %\
+                    (err_name, constants.ERR_CODES[err_name]))
+
+    from cStringIO import StringIO
+    pxd_out = open(fname, 'w')
+    extern_block = StringIO()
+    try:
+        for ctp in ctps:
+            for line in ctp.gen_pxd_cimports():
+                pxd_out.write(line+'\n')
+            for line in ctp.gen_pxd_intern_typedef():
+                pxd_out.write(line+'\n')
+        for ctp in ctps:
+            for line in ctp.gen_pxd_extern_typedef():
+                extern_block.write(INDENT+line+'\n')
+        for ctp in ctps:
+            for line in ctp.gen_pxd_extern_extra():
+                extern_block.write(INDENT+line+'\n')
+        extern_block = extern_block.getvalue()
+        if extern_block.rstrip():
+            pxd_out.write('cdef extern from "%s":\n' % h_name)
+            write_err_codes(pxd_out)
+            pxd_out.write(extern_block)
+    finally:
+        pxd_out.close()
+
+#------------------------------------------------------------------------------
+# -- Factory function; creates _ConfigTypeParam instances. --
 
 def ConfigTypeParam(basetype, odecl, fwrap_name, lang='fortran'):
     if lang == 'c':
@@ -237,6 +262,9 @@ class _CmplxTypeParam(_ConfigTypeParam):
 class _CConfigTypeParam(_ConfigTypeParam):
 
     lang = 'c'
+
+#------------------------------------------------------------------------------
+# -- Type mapping info. --
 
 f2c = {
     'c_int'             : 'int',
