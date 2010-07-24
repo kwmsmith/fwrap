b4daa1ff947770f62060e52a6d016ec3cf37f95f
Kurt Smith <kwmsmith@gmail.com>
2010-07-24 00:34:36 -0500

    improved fort_expr.py and included pyparsing source

--- fwrap/fort_expr.py
+++ fwrap/fort_expr.py
@@ -9,43 +9,103 @@
 #
 # Copyright 2003-2009 by Paul McGuire
 #
-from pyparsing import Literal,CaselessLiteral,Word,Group,Optional,\
-    ZeroOrMore,Forward,nums,alphas,Regex
+from pyparsing_py2 import Literal,CaselessLiteral,Word,Group,Optional,\
+    ZeroOrMore,Forward,nums,alphas,Regex,Combine,TokenConverter
 import math
 import operator
 
-stack = []
+class exprprinter(object):
 
-def pushInt(strg, loc, toks):
-    # import pdb; pdb.set_trace()
-    pass
+    def __init__(self, expr):
+        self.expr = expr
+        self.seen = []
+        self.ident = '    '
 
-def pushReal(strg, loc, toks):
-    # import pdb; pdb.set_trace()
-    pass
+    def visit(self):
+        return self._visit(self.expr, indent=0)
 
-def pushCmplx(strg, loc, toks):
-    # import pdb; pdb.set_trace()
-    pass
+    def _visit(self, node, indent):
+        if isinstance(node, str):
+            print self.ident*indent, repr(node)
+            return
+        elif node == None:
+            return
+        elif isinstance(node, list):
+            for n in node:
+                self._visit(n, indent)
+            return
 
-def pushChar(strg, loc, toks):
-    # import pdb; pdb.set_trace()
-    pass
+        try:
+            node.children
+        except AttributeError:
+            print self.ident*indent, node
+            return
+
+        for childname in node.children:
+            print self.ident*indent, childname
+            child = node.children[childname]
+            self._visit(child, indent+1)
+
+class exprnode(object):
+
+    children = []
+
+    def __init__(self, s, loc, toks):
+        self.expr = toks.asList()[:]
+        self.children = {"expr" : self.expr}
+
+    def __repr__(self):
+        return repr(self.children)
+
+class reallitconst(exprnode):
+
+    def __init__(self, s, loc, toks):
+        self.sign, self.real, self.kind = None, None, None
+        toks = toks.asList()
+        if toks[0] in "+-":
+            self.sign, self.real = toks[0], toks[1]
+        else:
+            self.sign, self.real = None, toks[0]
+        if len(toks) > 2:
+            under, self.kind = toks[-2:]
+        else:
+            assert len(toks) < 4
+        self.children = {"sign" : self.sign, "real" : self.real,
+                         "kind" : self.kind}
+
+class funcrefnode(exprnode):
+
+    def __init__(self, s, loc, toks):
+        self.name, self.arg_spec_list = toks.asList()[0], toks.asList()[1:]
+        self.children = {"name" : self.name, "args" : self.arg_spec_list}
+
+class funcarg(exprnode):
+
+    def __init__(self, s, loc, toks):
+        self.expr = toks.asList()[:]
+        self.children = {"expr" : self.expr}
+
+class argspec(exprnode):
+     
+    def __init__(self, s, loc, toks):
+        self.kw = None
+        if len(toks) == 3:
+            self.kw, eq, self.arg = toks.asList()
+        else:
+            self.arg, = toks.asList()
+        self.children = {"kw" : self.kw, "arg" : self.arg}
+
+class argspeclist(exprnode):
+
+    def __init__(self, s, loc, toks):
+        self.arg_spec_list = toks.asList()[:]
+        self.children = {"arg_list" : self.arg_spec_list}
 
 fort_expr_bnf = None
 def get_fort_expr_bnf():
     global fort_expr_bnf
     if fort_expr_bnf:
         return fort_expr_bnf
-
-    def _re_grp(s):
-        return r"(:?%s)" % s
-
-    def _re_opt(s):
-        return _re_grp(s) + r"?"
-
-    def _re_or(s1, s2):
-        return r"(:?%s|%s)" % (s1, s2)
 
     expr = Forward()
 
@@ -59,64 +119,47 @@ def get_fort_expr_bnf():
     comma = Literal(",")
     concat_op = Literal("//")
     power_op = Literal("**")
+    expo_letter = Regex(r"[eEdD]")
     mult_op = (Literal("*") | Literal("/"))
     sign = (Literal("+") | Literal("-"))
     add_op = sign
 
-    name_re = r"[a-zA-Z]\w*"
-    name = Regex(name_re)
+    name = Regex(r"[a-zA-Z]\w*")
 
     # int literals
-    digit_string_re = _re_grp(r"[0-9]+")
-    digit_string = Regex(digit_string_re)
-    opt_sign_re = _re_grp(r"[+-]?")
+    digit_string = Word(nums)
+    signed_digit_string = Combine(Optional(sign) + digit_string)
 
-    signed_digit_string_re = opt_sign_re + digit_string_re
-    signed_digit_string = Regex(signed_digit_string_re)
+    kind_param = (digit_string | name)
 
-    kind_param_re = _re_or(digit_string_re, name_re)
-    kind_param = Regex(kind_param_re)
+    int_literal_constant = ((digit_string + Optional("_" + kind_param)) | name)
+    signed_int_literal_constant = (Optional(sign) + int_literal_constant)
 
-    under_re = r"_"
-    _1st_re = digit_string_re + _re_opt(under_re +  kind_param_re)
-    int_literal_constant_re = _re_or(_1st_re, name_re)
-    signed_int_literal_constant_re = _re_grp(opt_sign_re + int_literal_constant_re)
-    print signed_int_literal_constant_re
-    int_literal_constant = Regex(int_literal_constant_re)
-    signed_int_literal_constant = Regex(signed_int_literal_constant_re).setParseAction(pushInt)
+    exponent = signed_digit_string
 
-    # real literals
-    exponent_re = signed_digit_string_re
-    expo_letter_re = _re_grp(r"[eEdD]")
-    sig1st_re = _re_grp(digit_string_re + r"\." + _re_opt(digit_string_re))
-    sig2nd_re = _re_grp(r"\." + digit_string_re)
-    significand_re = _re_or(sig1st_re, sig2nd_re)
-    real_lit_const_1st_re = _re_grp(
-                                significand_re +
-                                _re_opt(
-                                     expo_letter_re + exponent_re) +
-                                _re_opt(under_re + kind_param_re))
-    real_lit_const_2nd_re = _re_grp(
-                                digit_string_re + expo_letter_re + exponent_re +
-                                _re_opt(under_re + kind_param_re))
-    real_literal_constant_re = _re_or(real_lit_const_1st_re, real_lit_const_2nd_re)
-    real_literal_constant = Regex(real_literal_constant_re)
-    signed_real_literal_constant_re = _re_grp(opt_sign_re + real_literal_constant_re)
-    print signed_real_literal_constant_re
-    signed_real_literal_constant = Regex(signed_real_literal_constant_re).setParseAction(pushReal)
+    # real literal
+    sig1st = (digit_string + dot + Optional(digit_string))
+    sig2nd = (dot + digit_string)
+    significand = (sig1st | sig2nd)
+    real_lit_const_1st = (Combine(significand + Optional(expo_letter + exponent)) +
+                                Optional(under + kind_param))
+    real_lit_const_2nd = (Combine(digit_string + expo_letter + exponent) +
+                                Optional(under + kind_param))
+    real_literal_constant = (real_lit_const_1st | real_lit_const_2nd)
+    signed_real_literal_constant = (Optional(sign) + real_literal_constant).setParseAction(reallitconst)
 
     # complex literals
     cmplx_part = ( signed_real_literal_constant
                   | signed_int_literal_constant
                   | name)
-    complex_literal_constant = (lpar + cmplx_part + comma + cmplx_part + rpar).setParseAction(pushCmplx)
+    complex_literal_constant = (lpar + cmplx_part + comma + cmplx_part + rpar)
 
     # character literals
     rep_char = (Literal('""') | Literal("''") | Regex(r"[a-zA-Z0-9]"))
     char_literal_constant = ( (Optional(kind_param + under) +
                                 squote + ZeroOrMore(rep_char) + squote)
                               | (Optional(kind_param + under) +
-                                dquote + ZeroOrMore(rep_char) + dquote)).setParseAction(pushChar)
+                                dquote + ZeroOrMore(rep_char) + dquote))
 
     # logical literals
     logical_literal_constant = (  CaselessLiteral(".TRUE.") + Optional(under + kind_param)
@@ -129,13 +172,13 @@ def get_fort_expr_bnf():
                 | signed_int_literal_constant)
 
     arg = (expr | name)
-    arg_spec = (Optional(name + Literal("=")) + arg)
+    arg_spec = (Optional(name + Literal("=")) + arg).setParseAction(argspec)
     arg_spec_list = (arg_spec + ZeroOrMore(comma + arg_spec))
-    function_reference = (name + Group(lpar + Optional(arg_spec_list) + rpar))
+    function_reference = (name + lpar + Optional(arg_spec_list)
+                                    + rpar).setParseAction(funcrefnode)
 
     #R701
-    # primary = (ZeroOrMore(sign) + (function_reference | constant | name) | Group(lpar + expr + rpar))
-    primary = (ZeroOrMore(sign) + (function_reference | constant | name | Group(lpar + expr + rpar)))
+    primary = (function_reference ^ constant ^ name ^ Group(lpar + expr + rpar))
 
     #R702 we ignore defined unary ops for now.
     level1_expr = primary
@@ -144,7 +187,7 @@ def get_fort_expr_bnf():
     mult_operand = Forward()
     mult_operand << (level1_expr + Optional((power_op + mult_operand)))
     add_operand = (mult_operand + ZeroOrMore(mult_op + mult_operand))
-    level2_expr = (add_operand + ZeroOrMore(add_op + add_operand))
+    level2_expr = (ZeroOrMore(sign) + add_operand + ZeroOrMore(add_op + add_operand))
 
     #R710 - R711
     level3_expr = (level2_expr + ZeroOrMore(concat_op + level2_expr))
@@ -152,107 +195,87 @@ def get_fort_expr_bnf():
     # We skip level 4 and level 5 expressions, since they aren't valid in a
     # dimension or ktp context.
 
-    expr << level3_expr
+    expr << level3_expr.setParseAction(exprnode)
 
     fort_expr_bnf = expr
     return fort_expr_bnf
 
+def parser(s):
+    return get_fort_expr_bnf().parseString(s, parseAll=True)
 
-# bnf = None
-# def BNF():
-    # """
-    # expop   :: '**'
-    # multop  :: '*' | '/'
-    # addop   :: '+' | '-'
-    # integer :: ['+' | '-'] '0'..'9'+
-    # atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
-    # factor  :: atom [ expop factor ]*
-    # term    :: factor [ multop factor ]*
-    # expr    :: term [ addop term ]*
-    # """
-    # global bnf
-    # if not bnf:
-        # point = Literal( "." )
-        # e     = CaselessLiteral( "E" )
-        # #~ fnumber = Combine( Word( "+-"+nums, nums ) +
-                           # #~ Optional( point + Optional( Word( nums ) ) ) +
-                           # #~ Optional( e + Word( "+-"+nums, nums ) ) )
-        # fnumber = Regex(r"[+-]?\d+(:?\.\d*)?(:?[eE][+-]?\d+)?")
-        # ident = Word(alphas, alphas+nums+"_$")
+from nose.tools import eq_, ok_
 
-        # plus  = Literal( "+" )
-        # minus = Literal( "-" )
-        # mult  = Literal( "*" )
-        # div   = Literal( "/" )
-        # lpar  = Literal( "(" ).suppress()
-        # rpar  = Literal( ")" ).suppress()
-        # addop  = plus | minus
-        # multop = mult | div
-        # expop = Literal( "**" )
-        # pi    = CaselessLiteral( "PI" )
+class test_fort_expr(object):
 
-        # expr = Forward()
-        # # atom = (Optional("-") + ( pi | e | fnumber | ident + lpar + expr +
-        # # rpar ).setParseAction( pushFirst ) |
-        # atom = (Optional("-") + ( pi | e | fnumber | ident + lpar + expr + rpar ) |
-                # Group( lpar + expr + rpar ))
+    def test_int_literal(self):
+        # eq_(parser("9").asList(), ['9'])
+        print parser("9").asDict()
+        s = str(1234567890)
+        print parser(s).asDict()
+        # eq_(parser(s).asList(), [s])
 
-        # # by defining exponentiation as "atom [ ^ factor ]..." instead of "atom
-        # # [ ^ atom ]...", we get right-to-left exponents, instead of
-        # # left-to-right
-        # # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
-        # factor = Forward()
-        # factor << (atom + ZeroOrMore( ( expop + factor )))
+    def test_signed_int_lit(self):
+        istr = str(310130813080138)
+        s = "+%s" % istr
+        print parser(s).asDict()
+        # eq_(parser(s).asList(), ['+', istr])
+        skp = "%s_8 + 10_abc" % s
+        print parser(skp).asDict()
+        print parser(skp).asList()
 
-        # term = factor + ZeroOrMore( ( multop + factor ))
-        # expr << (term + ZeroOrMore( ( addop + term )))
-        # bnf = expr
-    # return bnf
 
-if __name__ == "__main__":
+    def test_func_ref(self):
+        ss = "foo(a, b-3+x(14), c=d+1)"
+        expr = parser(ss).asList()[0]
+        exprprinter(expr).visit()
 
-    def test(s):
-        global exprStack
-        results = get_fort_expr_bnf().parseString(s)
-        print s, results
+    def test_reallitconst(self):
+        ss = "-+12354.5678E-12_aoeu"
+        expr = parser(ss).asList()[0]
+        exprprinter(expr).visit()
 
-    test("9")
-    test("-9")
-    test("3.1415926")
-    test("3.1415926E10")
-    test("3.1415926_8")
-    test("--9")
-    test("-E")
-    test("9 + 3 + 6")
-    test("9 + 3 / 11")
-    test("(9 + 3)")
-    test("(9+3) / 11")
-    test("9 - 12 - 6")
-    test("9 - (12 - 6)")
-    test("2*3.14159")
-    test("3.1415926535*3.1415926535 / 10")
-    test("PI * PI / 10")
-    test("PI*PI/10")
-    test("PI**2")
-    test("round(PI**2)")
-    test("6.02E23 * 8.048")
-    test("e / 3")
-    test("sin(PI/2)")
-    test("trunc(E)")
-    test("trunc(-E)")
-    test("round(E)")
-    test("round(-E)")
-    test("E**PI")
-    test("2**3**2")
-    test("2**3+2")
-    test("2**3+5")
-    test("2**9")
-    test("sgn(-2)")
-    test("sgn(0)")
-    test("sgn(0.1)")
-    test("(0.0)")
-    test("(abc, def)")
-    test("(0.0, 0.0)")
-    test("3 -- (+9)")
-    test("3 --+--(+9)")
-    test("kind('a')")
+        ss2 = ".02808_a123_45"
+        expr = parser(ss2).asList()[0]
+        exprprinter(expr).visit()
+
+
+# test("9")
+# test("-9")
+# test("3.1415926")
+# test("3.1415926E10")
+# test("3.1415926_8")
+# test("--9")
+# test("-E")
+# test("9 + 3 + 6")
+# test("9 + 3 / 11")
+# test("(9 + 3)")
+# test("(9+3) / 11")
+# test("9 - 12 - 6")
+# test("9 - (12 - 6)")
+# test("2*3.14159")
+# test("3.1415926535*3.1415926535 / 10")
+# test("PI * PI / 10")
+# test("PI*PI/10")
+# test("PI**2")
+# test("round(PI**2)")
+# test("6.02E23 * 8.048")
+# test("e / 3")
+# test("sin(PI/2)")
+# test("trunc(E)")
+# test("trunc(-E)")
+# test("round(E)")
+# test("round(-E)")
+# test("E**PI")
+# test("2**3**2")
+# test("2**3+2")
+# test("2**3+5")
+# test("2**9")
+# test("sgn(-2)")
+# test("sgn(0)")
+# test("sgn(0.1)")
+# test("(0.0)")
+# test("(abc, def)")
+# test("(0.0, 0.0)")
+# test("3 -(-(+9))")
+# test("3 -(-(+(-(-(+9)))))")
+# test("kind('a')")
