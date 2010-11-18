1b5d115ba6f1995c41d249a80695d9318a414ee0
Dag Sverre Seljebotn <dagseljebotn@gmail.com>
2010-11-18 12:12:01 +0100

    Refactor basic git process calling from version.py

---
+++ fwrap/git.py
@@ -0,0 +1,22 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2010 Kurt Smith, Dag Sverre Seljebotn
+# All rights reserved. See LICENSE.txt.
+#------------------------------------------------------------------------------
+
+# Simple wrapper around command-line git
+from subprocess import Popen, PIPE
+
+def execproc(cmd):
+    # TODO: Spaces in filenames
+    pp = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
+    pp.wait()
+    result = pp.stdout.read().strip()
+    err = pp.stderr.read()
+    return result
+
+def execproc_with_default(cmd, default):
+    try:
+        return execproc(cmd)
+    except OSError:
+        return default
+
--- fwrap/version.py
+++ fwrap/version.py
@@ -5,23 +5,16 @@
 
 import os
 from os import path
+from fwrap.git import execproc_with_default
 
 # Set isrelease = True for release version.
 isrelease = False
 base_version = "0.2.0"
 
 def get_version():
-
     if isrelease: return base_version
-
     from subprocess import Popen, PIPE
     git_dir = path.join(path.dirname(path.dirname(__file__)), '.git')
     cmd = "git --git-dir=%s rev-parse --short HEAD" % git_dir
-    try:
-        pp = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
-        pp.wait()
-        global_id = pp.stdout.read().strip()
-        err_txt = pp.stderr.read()
-    except OSError:
-        global_id = "unknown"
+    global_id = execproc_with_default(cmd, "unknown")
     return "%sdev_%s" % (base_version, global_id)
